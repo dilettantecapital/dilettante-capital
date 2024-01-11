a34090a296f48e25fccb4f5f2a3a72232834a149
Shane Drabing <shane.drabing@gmail.com>
2024-01-11 11:28:27 -0700

    constants

---
+++ posts/20240114/constants.py
@@ -0,0 +1,49 @@
+import math
+from decimal import Decimal
+
+
+# FUNCTIONS
+
+
+def recurse_phi(n):
+    phi = Decimal(1)
+    for i in range(int(n)):
+        phi = 1 + 1 / phi
+    return phi
+
+
+def recurse_e(n):
+    e = Decimal(1)
+    for i in range(int(n)):
+        e += Decimal(1) / math.factorial(1 + i)
+    return e
+
+
+def newtons_root(x, y = None, limit = 995):
+    if y is None:
+        y = Decimal(x)
+    if x == (y * y) or limit <= 0:
+        return y
+    else:
+        return newtons_root(x, (y + x / y) / 2, limit - 1)
+
+
+def test_precision(f, start = 1, limit = 1e6):
+    hold = 0
+    for n in range(int(start), int(limit)):
+        test = f(n)
+        if test == hold:
+            break
+        hold = test
+    print(n, test)
+
+
+# SCRIPT
+
+
+if __name__ == "__main__":
+    test_precision(recurse_phi)
+    test_precision(recurse_e)
+
+    x = newtons_root(47)
+    print(x * x)
